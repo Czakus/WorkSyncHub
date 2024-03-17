9835f677e4d39257c5b217f8d2b7375e4eed3188
Czakus <krusik.szymon@gmail.com>
2024-03-17 22:05:20 +0100

    feat(auth): Create logout button

--- src/app/modules/application/components/side-menu/side-menu.component.ts
+++ src/app/modules/application/components/side-menu/side-menu.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { UserService } from "../../../auth/services/user.service";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { SignOutAction } from "../../../auth/store/actions/sign-out.actions";
 
 @Component({
   selector: 'wsh-side-menu',
@@ -10,9 +15,21 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 
   imports: [
     CommonModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatButtonModule
   ]
 })
 export class SideMenuComponent {
+
+  userService = inject(UserService);
+  router = inject(Router);
+  store = inject(Store);
+
+
+  logout() {
+    this.router.navigate(['/']);
+    this.userService.clearUser();
+    this.store.dispatch(SignOutAction());
+  }
 
 }
