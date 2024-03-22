2538ddeef1cbd997d05363f76e5c880310e14452
Czakus <krusik.szymon@gmail.com>
2024-03-22 20:53:26 +0100

    feat(auth): Change layout of auth page

--- src/app/modules/auth/store/effects/refresh-token.effects.ts
+++ src/app/modules/auth/store/effects/refresh-token.effects.ts
@@ -1,25 +1,30 @@
-import { Injectable, inject } from "@angular/core";
-import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { RefreshTokenAction, RefreshTokenFailureAction, RefreshTokenSuccessAction } from "../actions/refresh-token.actions";
-import { catchError, from, map, of, switchMap } from "rxjs";
-import { RefreshTokenService } from "../../services/refresh-token.service";
-import { AuthFacade } from "../auth.facade";
+import { Injectable, inject } from '@angular/core';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import {
+  RefreshTokenAction,
+  RefreshTokenFailureAction,
+  RefreshTokenSuccessAction,
+} from '../actions/refresh-token.actions';
+import { catchError, from, map, of, switchMap } from 'rxjs';
+import { RefreshTokenService } from '../../services/refresh-token.service';
+import { AuthFacade } from '../auth.facade';
 
 @Injectable()
 export class RefreshTokenEffects {
+
   actions$ = inject(Actions);
   refreshTokenService = inject(RefreshTokenService);
   authFacade = inject(AuthFacade);
 
-  $refreshToken = createEffect(() => {
+  refreshToken$ = createEffect(() => {
     return this.actions$.pipe(
       ofType(RefreshTokenAction),
       switchMap((action) =>
         from(this.refreshTokenService.refreshToken(action.user)).pipe(
-          map((token) => RefreshTokenSuccessAction({token})),
+          map((token) => RefreshTokenSuccessAction({ token })),
           catchError((error) => of(RefreshTokenFailureAction({ error })))
         )
       )
-    )
-  })
+    );
+  });
 }
